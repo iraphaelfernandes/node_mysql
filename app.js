7bd39c212b0ac915fa224ee5fd88883ac82ad92b
Raphael Fernandes <raphael.fernandes.eng@gmail.com>
2020-06-28 17:10:49 -0300

    Not working

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const pagamento = require('./models/Pagamento');
+const moment = require('moment');
+const Pagamento = require('./models/Pagamento');
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers:{
+    formatDate: (date) => {
+      return moment(date).format('DD/MM/YYYY')
+    }
+  }
+}));
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +20,9 @@ app.use(bodyParser.json());
 
 //Rotas
 app.get('/pagamento', (req, res) => {
+  Pagamento.findAll().then( (pagamentos)=>{
+  res.render('pagamento', {pagamentos:pagamentos});
+  })
   res.render('pagamento')
 });
 
@@ -20,7 +31,7 @@ app.get('/cad-pagamento', (req, res) => {
 });
 
 app.post('/add-pagamento', (req, res) => {
-  pagamento.create({
+  Pagamento.create({
       nome: req.body.nome,
       valor: req.body.valor
     }).then(()=>{
